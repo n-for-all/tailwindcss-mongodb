a22462edc7104eeb7bbfbf5d44f73e7f7fdf4166
Naji Amer <icu090@gmail.com>
2022-10-21 16:17:23 +0400

    0.0.4

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = plugin.withOptions(
       path = 'safelist.txt',
       callback = null,
       uri = 'mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority',
+      options = {},
     }) =>
     async ({ theme }) => {
       if (!callback) {
@@ -15,12 +16,15 @@ module.exports = plugin.withOptions(
         )
         return
       }
-      const client = new MongoClient(uri)
+      const client = new MongoClient(uri, options)
       try {
         await client.connect()
-        let value = await callback(client)
-        fs.writeFileSync(path, value.join('\n'))
-        await client.disconnect()
+        try {
+          let value = await callback(client)
+          fs.writeFileSync(path, value.join('\n'))
+        } catch (e) {
+          console.error('Tailwind Mongodb Plugin Callback Error: ', e)
+        }
       } catch (e) {
         console.error('Tailwind Mongodb Plugin: ', e)
       } finally {
